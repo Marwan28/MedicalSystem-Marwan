b80911f1de677a6f2bffc5e4e449cf572f2243bb
Marwan <marwanabdelwahab28@gmail.com>
2022-03-15 19:58:13 +0200

    finish receptionist

--- app/src/main/java/com/example/medicalui/MainActivity.java
+++ app/src/main/java/com/example/medicalui/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doctor(View view) {
-
+        firstSplashScreen();
     }
 
     public void receptionist(View view) {
